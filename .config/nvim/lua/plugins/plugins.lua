a2a826e55ced2903487b386a593f6615a1e6ea70
AutumnOctJXS <autumn10140310@gmail.com>
2024-04-24 17:26:29 +0900

    add vimtex

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -36,4 +36,13 @@ return{
         -- lexima.vim 括弧補完
         'cohama/lexima.vim',
     },
+    {
+        -- vimtex TeX編集補助
+        'lervag/vimtex',
+        lazy = false,     -- we don't want to lazy load VimTeX
+        -- tag = 'v2.15', -- uncomment to pin to a specific release
+        init = function()
+        -- VimTeX configuration goes here
+        end
+    },
 }
